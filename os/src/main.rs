24d9fdfb58534f8204ff557d163839b36907604a
leaf <1280760251@qq.com>
2026-05-09 12:00:43 +0800

    23301139-徐健智-实验一-独立的可执行程序

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,9 +1,70 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
+use core::fmt::{self, Write};
 use core::panic::PanicInfo;
+
+const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        asm!("ecall",
+             in("x10") args[0],
+             in("x11") args[1],
+             in("x12") args[2],
+             in("x17") id,
+             lateout("x10") ret
+        );
+    }
+    ret
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    };
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn _start() {
+    println!("Hello, world!");
+    println!("23301139 - 徐健智");
+    sys_exit(9);
 }
