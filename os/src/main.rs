cbea9331cbff25e05fe6f08add5f25d1487645a7
leaf <1280760251@qq.com>
2026-05-11 11:59:13 +0800

    23301139-徐健智-实验二大致完成

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,70 +1,52 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
+#[macro_use]
 
-use core::arch::asm;
-use core::fmt::{self, Write};
-use core::panic::PanicInfo;
+mod console;
+mod lang_items;
+mod sbi;
 
-const SYSCALL_EXIT: usize = 93;
-const SYSCALL_WRITE: usize = 64;
+use core::arch::global_asm;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+global_asm!(include_str!("entry.asm"));
 
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
+fn clear_bss() {
+    unsafe extern "C" {
+        fn sbss();
+        fn ebss();
+    }
     unsafe {
-        asm!("ecall",
-             in("x10") args[0],
-             in("x11") args[1],
-             in("x12") args[2],
-             in("x17") id,
-             lateout("x10") ret
-        );
+        (sbss as usize..ebss as usize).for_each(|a| {
+            (a as *mut u8).write_volatile(0)
+        });
     }
-    ret
-}
-
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
-}
-
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
-}
-
-struct Stdout;
-
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        sys_write(1, s.as_bytes());
-        Ok(())
-    }
-}
-
-pub fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
-}
-
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::print(format_args!($fmt $(, $($arg)+)?));
-    }
-}
-
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    };
 }
 
 #[unsafe(no_mangle)]
-extern "C" fn _start() {
+pub fn rust_main() -> ! {
+    unsafe extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
+    clear_bss();
     println!("Hello, world!");
-    println!("23301139 - 徐健智");
-    sys_exit(9);
+    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    println!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
 }
