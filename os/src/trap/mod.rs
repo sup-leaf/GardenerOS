7aa4647423cbdfdf1597a274aad37969e9ab1e86
leaf <1280760251@qq.com>
2026-06-07 19:15:02 +0800

    23301139-徐健智-实验五-分时多任务与抢占式调度

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -4,8 +4,8 @@ use core::arch::global_asm;
 use riscv::register::{stvec, scause, stval};
 use riscv::register::stvec::Stvec;
 use crate::syscall::syscall;
-// use crate::batch::run_next_app;
-use crate::task::exit_current_and_run_next;
+use crate::task::{exit_current_and_run_next, suspend_current_and_run_next};
+use crate::timer::set_next_trigger;
 
 global_asm!(include_str!("trap.S"));
 
@@ -23,7 +23,7 @@ pub fn init() {
 pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
     let scause_val = scause::read().bits();
     let stval_val = stval::read();
-    
+
     match scause_val {
         8 => {
             cx.sepc += 4;
@@ -37,9 +37,17 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
             println!("[kernel] IllegalInstruction in application, core dumped.");
             exit_current_and_run_next();
         }
+        0x8000000000000005 => {
+            set_next_trigger();
+            suspend_current_and_run_next();
+        }
         _ => {
             panic!("Unsupported trap {}, stval = {:#x}!", scause_val, stval_val);
         }
     }
     cx
+}
+
+pub fn enable_timer_interrupt() {
+    unsafe { riscv::register::sie::set_stimer(); }
 }
--- user/src/lib.rs
+++ user/src/lib.rs
@@ -36,3 +36,7 @@ fn main() -> i32 {
 }
 
 pub fn yield_() -> isize { sys_yield() }
+
+pub fn get_time() -> isize {
+    sys_get_time()
+}
